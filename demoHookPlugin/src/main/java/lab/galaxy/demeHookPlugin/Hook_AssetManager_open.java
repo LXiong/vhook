8e6672bd45b3a17b1549c7fbab28579add96d5a6
liupeng110 <717219917@qq.com>
2017-08-18 17:27:24 +0800

    Initial commit

---
+++ demoHookPlugin/src/main/java/lab/galaxy/demeHookPlugin/Hook_AssetManager_open.java
@@ -0,0 +1,21 @@
+package lab.galaxy.demeHookPlugin;
+
+import android.util.Log;
+
+import java.io.InputStream;
+
+
+public class Hook_AssetManager_open {
+    public static String className = "android.content.res.AssetManager";
+    public static String methodName = "open";
+    public static String methodSig = "(Ljava/lang/String;)Ljava/io/InputStream;";//参数为string 包名java.io.inputstream
+    public static InputStream hook(Object thiz, String fileName) {
+        Log.w("YAHFA", "open asset "+fileName);
+        return origin(thiz, fileName);
+    }
+
+    public static InputStream origin(Object thiz, String msg) {
+        Log.w("YAHFA", "should not be here");
+        return null;
+    }
+}
